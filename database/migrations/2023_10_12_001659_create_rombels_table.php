105a8091c8f279c0678d8c0859caf398bf4b310c
najwa <najwalatifa1812@gmail.com>
2023-10-17 15:27:53 +0700

    rombel

--- database/migrations/2023_10_12_001659_create_rombels_table.php
+++ database/migrations/2023_10_12_001659_create_rombels_table.php
@@ -12,7 +12,7 @@ return new class extends Migration {
     {
         Schema::create('rombels', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100)->unique();
+            $table->string('name', 50);
             $table->foreignId('jurusan_id');
             $table->timestamps();
         });
